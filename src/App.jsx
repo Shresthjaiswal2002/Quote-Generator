3b4724d612f75655a0e700fe0112da11c6579c40
Shresth Jaiswal <shresthjaiswal2002@gmail.com>
2023-02-03 10:06:50 +0530

    Quote

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {useEffect,useState} from 'react';
+import './App.css';
+
+function App() {
+  const[quote,setQuote]=useState("")
+  const getQuote=()=> {
+    fetch("https://api.quotable.io/random")
+     .then((response)=>response.json())
+    .then((data)=>setQuote(data))
+  }
+  
+function changeColor() {
+  document.body.style.backgroundColor=
+  'rgb('+Math.round(Math.random()*255)+
+  ','+Math.round(Math.random()*255)+','
+    +Math.round(Math.random()*255)+')';
+   
+}
+
+
+  useEffect(()=>getQuote(),[])
+  return (
+    <div className="App" >
+      <span id='Text'>{quote.content}</span>
+      <h4 className='Colorchange'>{quote.author}</h4>
+      <button onClick={getQuote}{...changeColor()}>Get Quote</button>
+    </div>
+  );
+}
+
+export default App;
